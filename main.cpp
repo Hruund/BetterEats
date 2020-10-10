99a010b2738c38062a5903c7bc63f0ec46044371
Lucasvct <lucas.victorion@gmail.com>
2020-10-10 15:58:56 +0200

    Changements esthétiques

--- main.cpp
+++ main.cpp
@@ -6,58 +6,53 @@
 #include <sstream>
 using namespace std;
 
-void menu_1(void);
-void menu_2(void);
-void quit(int);                //transforme une chaine de caractere en un float
-void creer_nouv_compte (void );
-void acces_compte(void);
-int appel_erreur (int);
-void affichage_resto();
-void pizza();
-void sushis();
-void burger();
-float recup;                                       
-string nom_compte_courant;
+void menu_1(void), menu_2(void), quit();
+void creer_nouv_compte (void), acces_compte(void);
+void affichage_resto(), pizza(), sushis(), burger();
+string plat;
 
 
 int main()
 {
+    cout << "-------------- Better Eats --------------\n\n";
     menu_1();
-    quit(1);
+    quit();
     return 0;
 }
     
 void menu_1()                                              
 {
     int choix;
-    cout << "MENU  :                 \n";
-    cout << "1-Creer nouveau compte  \n";
-    cout << "2-Acces compte existant \n";
-    cout << "3-Quit \n";
+    cout << "Menu principal :\n";
+    cout << "1 - Inscription  \n";
+    cout << "2 - Se connecter \n";
+    cout << "3 - Quitter \n";
     cout << "CHOIX : "; cin >> choix;
+    cout << "\n";
     switch(choix)
     {
     case 1 :
-            creer_nouv_compte();
+        creer_nouv_compte();
     case 2 :
-            acces_compte();
+        acces_compte();
     case 3 :
-            quit(1);
-    default: quit(1);
+        quit();
+    default: quit();
     }
 }
 
 void creer_nouv_compte()
 {
     string prenom, nom, mail, adresse, nom_complet, mdp;       //l'extension ".bank" est rajoutee au nom
-    cout << "Prénom : "; cin >> prenom;
+    cout << "Prenom : "; cin >> prenom;
     cout << "Nom : "; cin >> nom;
     cout << "Mail : "; cin >> mail;
     cout << "Adresse : "; cin >> adresse;
     cout << "Mot de passe : "; cin >> mdp;
     nom_complet = prenom + nom;
      
-    cout << "Compte crée ! \n";
+    cout << "Compte cree ! \n";
+    cout << "Bonjour " << prenom << " !\n\n";
     affichage_resto();
 }
      
@@ -67,62 +62,57 @@ void acces_compte()                                                             
     cout << "Mail: "; cin >> mail;
     cout << "Mot de passe : "; cin >> mdp;
     if(mail == "test@gmail.com" & mdp == "test"){
+        cout << "Bonjour Test ! Comment allez vous aujourd'hui ?\n\n";
         affichage_resto();
     }
     else {
-        appel_erreur (1);
-        cout << "Identifiants incorrects";
+        cout << "Identifiants incorrects, retour au menu\n\n";
         menu_1();
     }
 }
 
 void pizza(){
-    cout<< "Pizza \n ";
+    cout<< "Pizza fromage\n ";
+    cout<< "Pizza chorizo\n ";
+    cout<< "Pizza jambon\n ";
+    cout << "CHOIX : "; cin >> plat;
 }
 
 void sushis(){
     cout<< "California au saumon \n ";
+    cout<< "Sushis au saumon \n ";
+    cout << "CHOIX : "; cin >> plat;
 }
 
 void burger(){
     cout<< "Burger au Poulet \n ";
+    cout<< "Burger au Cantal \n ";
+    cout<< "Burger vegetarien \n ";
+    cout << "CHOIX : "; cin >> plat;
 }
 
 void affichage_resto(){
     int resto;
-    cout << "Restaurants :  \n";
-    cout << "1- Chez Luigi - Pizzeria  \n";
-    cout << "2- Lola sushi - Sushi \n";
-    cout << "3- King burger - Burger \n";
+    cout << "Un petit creux ? Choississez un restaurant ! \n";
+    cout << "1 - Chez Luigi - Pizzeria  \n";
+    cout << "2 - Lola sushi - Sushi \n";
+    cout << "3 - King burger - Burger \n";
     cout << "CHOIX : "; cin >> resto;
+    cout << "\n Tres bon choix ! Voici les plats disponibles : \n";
 
     switch(resto)
     {
     case 1 :
-            pizza();
+        pizza();
     case 2 :
-            sushis();
+        sushis();
     case 3 :
-            burger();
-    default: pizza();
+        burger();
+    default: quit();
     }
 }
 
-int appel_erreur(int erreur)
-{
-    if (erreur == 1)
-    {
-               cerr << "ERREUR: ACCES FICHIER IMPOSSIBLE";
-    }
-    if (erreur == 2)
-    {
-               cerr << "ERREUR: SAISIE INCORECTE";
-    }
-    return 1;                              
-}     
-
-
-void quit(int param)
+void quit()
 { 
-     exit(0);         
+    exit(0);         
 }
